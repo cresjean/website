eec1f64308147f1cdbf3e97103b1a4efe04522a1
Crespo Wang <crespo@crespowang.com>
2015-03-02 21:43:20 +1100

    SVG booster

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
         assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images'],
         patterns: {
             js: [
-                 [/(images\/.*?\.(?:gif|jpeg|jpg|png|webp))/gm, 'Update the JS to reference our revved images']
+                 [/(images\/.*?\.(?:gif|jpeg|jpg|png|svg|webp))/gm, 'Update the JS to reference our revved images']
             ]
         }
       }
